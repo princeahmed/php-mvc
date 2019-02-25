0ac34ac57b139af56e3139745df3592497046da4
Prince <israilahmed5@gmail.com>
2019-02-25 22:21:57 +0600

    Router match method added

--- Core/Router.php
+++ Core/Router.php
@@ -1,9 +1,13 @@
 <?php
 
 class Router {
+
 	protected $routes = [];
+	protected $params = [];
 
 	/**
+	 * Set routes
+	 *
 	 * @param array $routes
 	 */
 	public function setRoutes( $route, $params ) {
@@ -15,6 +19,30 @@ class Router {
 	 */
 	public function getRoutes() {
 		return $this->routes;
+	}
+
+	/**
+	 * Match the url
+	 * @param $url
+	 *
+	 * @return bool
+	 */
+	public function match($url){
+		foreach($this->routes as $route => $params){
+			if($route == $url){
+				$this->params = $params;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Get currently matched url params
+	 */
+
+	public function getParams(){
+		return $this->params;
 	}
 
 }
--- public/index.php
+++ public/index.php
@@ -1,1 +1,18 @@
-<?php 
+<?php
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+$router->setRoutes( '', [ 'controller' => 'Home', 'action' => 'index' ] );
+$router->setRoutes( 'posts', [ 'controller' => 'Posts', 'action' => 'index' ] );
+$router->setRoutes( 'users', [ 'controller' => 'Users', 'action' => 'index' ] );
+
+$url = $_SERVER['QUERY_STRING'];
+
+
+if($router->match($url)){
+	echo '<pre>';
+	    print_r($router->getParams());
+	echo '</pre>';
+}
