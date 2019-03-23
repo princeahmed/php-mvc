257c73ce9c2131cc6411196107a074b409f22aca
Prince <israilahmed5@gmail.com>
2019-03-24 00:04:50 +0600

    add card

--- App/Views/cache/670e192fca24f8968d1df20cb6500ad00c00f329.php
+++ App/Views/cache/670e192fca24f8968d1df20cb6500ad00c00f329.php
@@ -3,8 +3,9 @@
     <div class="section">
         <div class="container">
             <div class="row">
-                <div class="col-4">
-                    <div class="box">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam autem consectetur
+                <div class="col-8">
+                    <div class="box main">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam autem
+                        consectetur
                         eaque error eum
                         excepturi expedita laborum magni minus natus, nemo nulla odio, provident quae quam quasi quidem
                         quis
@@ -15,19 +16,30 @@
                     </div>
                 </div>
                 <div class="col-4">
-                    <div class="box">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam autem consectetur
-                        eaque error eum
-                        excepturi expedita laborum magni minus natus, nemo nulla odio, provident quae quam quasi quidem
-                        quis
-                        tempora.
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="box">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam autem consectetur
-                        eaque error eum
-                        excepturi expedita laborum magni minus natus, nemo nulla odio, provident quae quam quasi quidem
-                        quis
-                        tempora.
+                    <div class="box sidebar">
+                        <div class="card">
+                            <div class="card-left">
+                                <div class="label">
+                                    <a href="#" class="">PHP</a>
+                                </div>
+                                <div class="thumb">
+                                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Circle_Logo.svg/1200px-Circle_Logo.svg.png" alt="">
+                                </div>
+                                <div class="category">
+                                    <span>Intermediate Difficulty</span>
+                                </div>
+                            </div>
+                            <div class="card-right">
+                                <h3 class="title">Testing Prince</h3>
+                                <div class="content">
+                                    <p>Adipiscuibusdam quidem quis sequi sit soluta suscipit temporibus, vel veritatis vero?</p>
+                                </div>
+                                <div class="meta">
+                                    <span>5 Lessons</span>
+                                    <span>53:00 Min</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -39,42 +51,6 @@
     <div class="section">
         <div class="container">
             <div class="row">
-                <div class="col-1">
-                    <div class="box">Col-1</div>
-                </div>
-                <div class="col-2">
-                    <div class="box">col-2</div>
-                </div>
-                <div class="col-3">
-                    <div class="box">col-3</div>
-                </div>
-                <div class="col-4">
-                    <div class="box">col-4</div>
-                </div>
-                <div class="col-5">
-                    <div class="box">col-5</div>
-                </div>
-                <div class="col-6">
-                    <div class="box">col-6</div>
-                </div>
-                <div class="col-7">
-                    <div class="box">col-7</div>
-                </div>
-                <div class="col-8">
-                    <div class="box">col-8</div>
-                </div>
-                <div class="col-9">
-                    <div class="box">col-9</div>
-                </div>
-                <div class="col-10">
-                    <div class="box">col-10</div>
-                </div>
-                <div class="col-11">
-                    <div class="box">col-11</div>
-                </div>
-                <div class="col-12">
-                    <div class="box">col-12</div>
-                </div>
                 <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, asperiores doloribus
                     exercitationem libero magnam nobis quae quia sed voluptates voluptatum. Deserunt eaque eum, expedita
                     illo itaque odio quasi quis unde.
